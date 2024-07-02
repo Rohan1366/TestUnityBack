9d72af99d9ff20696fe938f77de46ba62ecc6746
Rohan Ghadge <113715810+Rohan1366@users.noreply.github.com>
2024-07-02 09:49:00 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const puppeteer = require('puppeteer');
 
 const app = express();
-const port = process.env.PORT || 3001;
+const port = 8080 || 8081;
 
 app.use(cors());
 app.use(express.json());
